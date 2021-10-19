ba4e1d7352e5c8a1703fab271d7ea0ea8dd9bd85
mvochteloo <martijn.vochteloo@gmail.com>
2021-10-19 12:12:31 +0200

    update on correlations with phenotype matrix

--- dev/plot_scripts/create_correlation_heatmap.py
+++ dev/plot_scripts/create_correlation_heatmap.py
@@ -69,9 +69,6 @@ class main():
         self.col_data_path = getattr(arguments, 'col_data')
         self.col_name = " ".join(getattr(arguments, 'col_name'))
         self.method = getattr(arguments, 'method')
-        self.call_rate = getattr(arguments, 'call_rate')
-        self.row_cluster = getattr(arguments, 'row_cluster')
-        self.col_cluster = getattr(arguments, 'col_cluster')
         self.out_filename = getattr(arguments, 'outfile')
 
         # Set variables.
@@ -128,20 +125,6 @@ class main():
                             choices=["Pearson", "Spearman"],
                             default="Spearman",
                             help="The correlation method. Default: Spearman.")
-        parser.add_argument("-cr",
-                            "--call_rate",
-                            type=float,
-                            default=0.01,
-                            help="The minimal call rate (not NaN values) for "
-                                 "calculating a correlation.")
-        parser.add_argument("-row_cluster",
-                            action='store_true',
-                            help="Cluster the rows."
-                                 " Default: False.")
-        parser.add_argument("-col_cluster",
-                            action='store_true',
-                            help="Cluster the rows."
-                                 " Default: False.")
         parser.add_argument("-o",
                             "--outfile",
                             type=str,
@@ -183,19 +166,15 @@ class main():
         col_df = col_df.loc[overlap, :]
 
         print("\tCorrelating.")
-        corr_df, sample_size_df = self.correlate(index_df=row_df, columns_df=col_df, triangle=triangle)
+        corr_df, pvalue_df = self.correlate(index_df=row_df, columns_df=col_df, triangle=triangle)
         print(corr_df)
-        print(sample_size_df)
+        print(pvalue_df)
 
-        print("\tClustering.")
-        if self.row_cluster:
-            corr_df = self.cluster(corr_df, axis=0)
-        if self.col_cluster:
-            corr_df = self.cluster(corr_df, axis=1)
+        print("\tMasking non-significant")
+        signif_df = self.mask_non_significant(df=corr_df, pvalue_df=pvalue_df)
 
         print("\tPlotting.")
-        self.plot_heatmap(corr_df=corr_df, xlabel=self.col_name, ylabel=self.row_name, appendix="correlations")
-        self.plot_heatmap(corr_df=sample_size_df, xlabel=self.col_name, ylabel=self.row_name, appendix="sample_sizes", vmax=len(overlap), as_int=True)
+        self.plot_heatmap(df=signif_df, annot_df=corr_df.round(2), xlabel=self.col_name, ylabel=self.row_name, appendix="correlations")
 
     @staticmethod
     def load_file(inpath, header, index_col, sep="\t", low_memory=True,
@@ -208,70 +187,64 @@ class main():
         return df
 
     def correlate(self, index_df, columns_df, triangle=False):
-        corr_df = pd.DataFrame(np.nan, index=index_df.columns, columns=columns_df.columns)
-        sample_size_df = pd.DataFrame(0, index=index_df.columns, columns=columns_df.columns)
+        index_df_n_nan_values = index_df.shape[0] - index_df.isna().sum(axis=0)
+        column_df_n_nan_values = columns_df.shape[0] - columns_df.isna().sum(axis=0)
 
-        for i, index_column in enumerate(index_df.columns):
-            for j, column_column in enumerate(columns_df.columns):
+        index_df_colnames = ["{} [N={:,}]".format(colname, index_df_n_nan_values[colname]) for colname in index_df.columns]
+        column_df_colnames = ["{} [N={:,}]".format(colname, column_df_n_nan_values[colname]) for colname in columns_df.columns]
+
+        corr_df = pd.DataFrame(np.nan, index=index_df_colnames, columns=column_df_colnames)
+        pvalue_df = pd.DataFrame(np.nan, index=index_df_colnames, columns=column_df_colnames)
+
+        for i, (index_column, index_colname) in enumerate(zip(index_df.columns, index_df_colnames)):
+            for j, (column_column, column_colname) in enumerate(zip(columns_df.columns, column_df_colnames)):
                 if triangle and i < j:
                     continue
                 corr_data = pd.concat([index_df[index_column], columns_df[column_column]], axis=1)
-                pre_filter_shape = corr_data.shape[0]
                 corr_data.dropna(inplace=True)
-                post_filter_shape = corr_data.shape[0]
 
                 coef = np.nan
-                if (post_filter_shape / pre_filter_shape) > self.call_rate and np.min(corr_data.std(axis=0)) > 0:
+                pvalue = np.nan
+                if np.min(corr_data.std(axis=0)) > 0:
                     if self.method == "Pearson":
-                        coef, _ = stats.pearsonr(corr_data.iloc[:, 1], corr_data.iloc[:, 0])
+                        coef, pvalue = stats.pearsonr(corr_data.iloc[:, 1], corr_data.iloc[:, 0])
                     elif self.method == "Spearman":
-                        coef, _ = stats.spearmanr(corr_data.iloc[:, 1], corr_data.iloc[:, 0])
+                        coef, pvalue = stats.spearmanr(corr_data.iloc[:, 1], corr_data.iloc[:, 0])
 
-                corr_df.loc[index_column, column_column] = coef
-                sample_size_df.loc[index_column, column_column] = post_filter_shape
+                corr_df.loc[index_colname, column_colname] = coef
+                pvalue_df.loc[index_colname, column_colname] = pvalue
 
-        return corr_df, sample_size_df
+        return corr_df, pvalue_df
 
     @staticmethod
-    def cluster(df, axis):
-        tmp = df.copy()
-        if axis == 1:
-            tmp = tmp.T
+    def mask_non_significant(df, pvalue_df, a=0.05):
+        signif_df = df.copy()
+        for i in range(signif_df.shape[0]):
+            for j in range(signif_df.shape[1]):
+                if np.isnan(pvalue_df.iloc[i, j]) or pvalue_df.iloc[i, j] >= a:
+                    signif_df.iloc[i, j] = 0
 
-        distances = pdist(tmp.values, metric='euclidean')
-        dist_matrix = squareform(distances)
-        linkage = hcl.linkage(squareform(dist_matrix), method="average")
-        Z = hcl.dendrogram(linkage, orientation='right')
-        index = Z['leaves']
+        return signif_df
 
-        if axis == 1:
-            tmp = df.iloc[:, index]
-        else:
-            tmp = tmp.iloc[index, :]
-
-        return tmp
-
-    def plot_heatmap(self, corr_df, xlabel="", ylabel="", appendix="", vmin=-1, vmax=1, as_int=False):
+    def plot_heatmap(self, df, annot_df, xlabel="", ylabel="", appendix="",
+                     vmin=-1, vmax=1):
         cmap = sns.diverging_palette(246, 24, as_cmap=True)
 
         fig, axes = plt.subplots(nrows=2,
                                  ncols=2,
-                                 figsize=(1 * corr_df.shape[1] + 5, 1 * corr_df.shape[0] + 5),
+                                 figsize=(1 * df.shape[1] + 10, 1 * df.shape[0] + 10),
                                  gridspec_kw={"width_ratios": [0.2, 0.8],
                                               "height_ratios": [0.8, 0.2]})
         sns.set(color_codes=True)
+
+        annot_df.fillna("", inplace=True)
 
         row_index = 0
         col_index = 0
         for _ in range(4):
             ax = axes[row_index, col_index]
             if row_index == 0 and col_index == 1:
-
-                annot_df = corr_df.round(2)
-                if as_int:
-                    annot_df = corr_df.astype(int)
-
-                sns.heatmap(corr_df, cmap=cmap, vmin=vmin, vmax=vmax, center=0,
+                sns.heatmap(df, cmap=cmap, vmin=vmin, vmax=vmax, center=0,
                             square=True, annot=annot_df, fmt='',
                             cbar=False, annot_kws={"size": 14, "color": "#000000"},
                             ax=ax)
@@ -292,7 +265,7 @@ class main():
                 col_index = 0
                 row_index += 1
 
-        # plt.tight_layout()
+        plt.tight_layout()
         fig.savefig(os.path.join(self.outdir, "{}_corr_heatmap_{}_{}.png".format(self.out_filename, self.method, appendix)))
         plt.close()
 
@@ -303,9 +276,6 @@ class main():
         print("  > Col data path: {}".format(self.col_data_path))
         print("  > Col name: {}".format(self.col_name))
         print("  > Correlation method: {}".format(self.method))
-        print("  > Call rate: {}".format(self.call_rate))
-        print("  > Row cluster: {}".format(self.row_cluster))
-        print("  > Col cluster: {}".format(self.col_cluster))
         print("  > Output filename: {}".format(self.out_filename))
         print("  > Outpath {}".format(self.outdir))
         print("")
